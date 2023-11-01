4847728a6f6861587b3a6783e3e4bd55b2f92702
Vernon Yang <vernon2gm@gmail.com>
2023-11-01 21:28:02 +0800

    Add swapout for thp and debug method
    Signed-off-by: Vernon Yang <vernon2gm@gmail.com>

--- swap/app.c
+++ swap/app.c
@@ -1,18 +1,94 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
 #include <sys/mman.h>
 
-int main(int argc, char *argv[])
+#define SHM_NAME "sync_status"
+
+static int pagesize;
+static char *buf;
+
+static inline void sync_start(void)
+{
+	int fd;
+
+	fd = shm_open(SHM_NAME, O_RDWR | O_CREAT, 0777);
+	ftruncate(fd, pagesize);
+
+	buf = mmap(NULL, pagesize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+}
+
+static inline void sync_end(void)
+{
+	munmap(buf, pagesize);
+	shm_unlink(SHM_NAME);
+}
+
+static void wait_shell_finish(void)
+{
+	memcpy(buf, "app2shell", 9);
+
+	while (strncmp(buf, "shell2app", 9))
+		sleep(1);
+}
+
+void swap_normal(void)
 {
 	char *buf;
 
-	buf = mmap(0, 4096, PROT_READ | PROT_WRITE,
-			MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	buf = mmap(0, pagesize, PROT_READ | PROT_WRITE,
+		   MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 
 	buf[0] = 't';
-	madvise(buf, 4096, MADV_PAGEOUT);
-	while(1);
 
-	munmap(buf, 4096);
+	wait_shell_finish();
+	madvise(buf, pagesize, MADV_PAGEOUT);
+	wait_shell_finish();
+
+	munmap(buf, pagesize);
+}
+
+static void __swap_thp(void *start_vaddr, int pagenum)
+{
+	char *buf;
+	int i;
+
+	buf = mmap(start_vaddr, pagesize * pagenum,
+		   PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+
+	for (i = 0; i < pagenum; i++)
+		buf[pagesize * i] = 0x12;
+
+	wait_shell_finish();
+	madvise(buf, pagesize * pagenum, MADV_PAGEOUT);
+	wait_shell_finish();
+
+	munmap(buf, pagesize * pagenum);
+}
+
+void swap_thp(void)
+{
+	__swap_thp((void *)0x7f987f200000, 512);
+}
+
+void swap_small_size_thp(void)
+{
+	__swap_thp((void *)0x7f9877ff0000, 16);
+}
+
+int main(int argc, char *argv[])
+{
+	pagesize = getpagesize();
+	sync_start();
+
+	swap_normal();
+	swap_thp();
+	swap_small_size_thp();
+
+	sync_end();
 
 	return 0;
 }
--- swap/zram_writeback_backingdev.sh
+++ swap/zram_writeback_backingdev.sh
@@ -8,14 +8,8 @@ echo 20M > /sys/block/zram0/disksize
 mkswap /dev/zram0
 swapon /dev/zram0
 
-cat /proc/swaps
-# Filename				Type		Size		Used		Priority
-# /dev/zram0                              partition	20476		0		-2
 gcc app.c
 ./a.out &
-cat /proc/swaps
-# Filename				Type		Size		Used		Priority
-# /dev/zram0                              partition	20476		256		-2
 
 cat /sys/kernel/debug/zram/zram0/block_state
 #            0           15.823322 ......
@@ -28,3 +22,5 @@ echo idle > /sys/block/zram0/writeback
 cat /sys/kernel/debug/zram/zram0/block_state
 #            0            0.000000 .w....
 #          256            0.000000 .w....
+
+swapoff /dev/zram0
