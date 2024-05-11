bed4965856f96afecc335ff14262f7884cda147c
Vernon Yang <vernon2gm@gmail.com>
2024-05-11 16:33:16 +0800

    Refine vmscan
    Signed-off-by: Vernon Yang <vernon2gm@gmail.com>

--- vmscan/app.c
+++ vmscan/app.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/mman.h>
+#include "dynamic_debug.h"
 
 #define PAGESIZE	4096
 
-static int get_file_pages(int fd)
+#define print_meminfo(format, ...)		\
+do {						\
+	printf(format, ##__VA_ARGS__);		\
+	system("cat /proc/meminfo | grep file");\
+} while(0)
+
+static inline int get_file_pages(int fd)
 {
 	struct stat st;
 
@@ -27,16 +35,30 @@ static void test_promote_active_list_from_read_syscall(void)
 	int nr_pages;
 	int fd;
 	int ret;
+	int i;
 
 	fd = open("testfile", O_RDONLY);
 	nr_pages = get_file_pages(fd);
-	while (nr_pages--) {
+
+	for (i = 0; i < nr_pages; i++) {
 		ret = read(fd, buf, PAGESIZE);
 		if (ret == -1) {
 			printf("Read file failed.\n");
 			break;
 		}
 	}
+	print_meminfo("meminfo after first read\n");
+
+	lseek(fd, 0, SEEK_SET);
+	for (i = 0; i < nr_pages; i++) {
+		ret = read(fd, buf, PAGESIZE);
+		if (ret == -1) {
+			printf("Read file failed.\n");
+			break;
+		}
+	}
+	print_meminfo("meminfo after second read\n");
+
 	close(fd);
 }
 
@@ -46,30 +68,50 @@ static void test_promote_active_list_from_write_syscall(void)
 	int nr_pages;
 	int fd;
 	int ret;
+	int i;
 
 	fd = open("testfile", O_WRONLY);
 	nr_pages = get_file_pages(fd);
-	while (nr_pages--) {
+
+	for (i = 0; i < nr_pages; i++) {
 		ret = write(fd, buf, PAGESIZE);
 		if (ret == -1) {
 			printf("Write file failed.\n");
 			break;
 		}
 	}
+	print_meminfo("meminfo after first write\n");
+
+	lseek(fd, 0, SEEK_SET);
+	for (i = 0; i < nr_pages; i++) {
+		ret = write(fd, buf, PAGESIZE);
+		if (ret == -1) {
+			printf("Write file failed.\n");
+			break;
+		}
+	}
+	print_meminfo("meminfo after second write\n");
+
 	close(fd);
 }
 
-static void test_promote_active_list_from_mmap_syscall(bool write)
+static void test_promote_active_list_from_mmap_syscall(bool share, bool write)
 {
 	char *buf;
-	char tmp;
+	int flags;
 	int fd;
 	int nr_pages, i;
+	char tmp;
 
 	fd = open("testfile", O_RDWR);
 	nr_pages = get_file_pages(fd);
-	buf = mmap(0, nr_pages * PAGESIZE, PROT_READ | PROT_WRITE,
-			MAP_PRIVATE | MAP_FILE, fd, 0);
+
+	if (share)
+		flags = MAP_FILE | MAP_SHARED;
+	else
+		flags = MAP_FILE | MAP_PRIVATE;
+
+	buf = mmap(0, nr_pages * PAGESIZE, PROT_READ | PROT_WRITE, flags, fd, 0);
 
 	for (i = 0; i < nr_pages; i++) {
 		if (write)
@@ -77,26 +119,53 @@ static void test_promote_active_list_from_mmap_syscall(bool write)
 		else
 			tmp = buf[i * PAGESIZE];
 	}
+	print_meminfo("meminfo after first %s %s\n", share ? "share" : "private",
+							write ? "write" : "read");
 
+	for (i = 0; i < nr_pages; i++) {
+		if (write)
+			buf[i * PAGESIZE] = 'b';
+		else
+			tmp = buf[i * PAGESIZE];
+	}
+	print_meminfo("meminfo after first %s %s\n", share ? "share" : "private",
+							write ? "write" : "read");
+
+	dynamic_debug_control("file memory.c +p");
 	munmap(buf, nr_pages * PAGESIZE);
+	dynamic_debug_control("file memory.c -p");
+	print_meminfo("meminfo after munmap\n");
+
 	close(fd);
 }
 
 int main(int argc, char *argv[])
 {
-	if (!argv[1])
+	dynamic_debug_start();
+
+	if (!argv[1]) {
+		printf("Empty options to execute default read() SYSCALL.\n");
 		goto DEFAULT;
+	}
 
 	if (!strncmp(argv[1], "read", strlen("read")))
 DEFAULT:	test_promote_active_list_from_read_syscall();
 	else if (!strncmp(argv[1], "write", strlen("write")))
 		test_promote_active_list_from_write_syscall();
-	else if (!strncmp(argv[1], "mmap_read", strlen("mmap_read")))
-		test_promote_active_list_from_mmap_syscall(0);
-	else if (!strncmp(argv[1], "mmap_write", strlen("mmap_write")))
-		test_promote_active_list_from_mmap_syscall(1);
-	else
+	else if (!strncmp(argv[1], "mmap_file_share_read", strlen("mmap_file_share_read")))
+		test_promote_active_list_from_mmap_syscall(1, 0);
+	else if (!strncmp(argv[1], "mmap_file_share_write", strlen("mmap_file_share_write")))
+		test_promote_active_list_from_mmap_syscall(1, 1);
+	else if (!strncmp(argv[1], "mmap_file_private_read", strlen("mmap_file_private_read")))
+		test_promote_active_list_from_mmap_syscall(0, 0);
+	else if (!strncmp(argv[1], "mmap_file_private_write", strlen("mmap_file_private_write")))
+		test_promote_active_list_from_mmap_syscall(0, 1);
+	else {
+		printf("Not exit options to execute default read() SYSCALL.\n");
 		goto DEFAULT;
+	}
+
+	dynamic_debug_end();
 
 	return 0;
 }
--- vmscan/test.sh
+++ vmscan/test.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-if [[ $1 == "read" || $1 == "write" || $1 == "mmap_read" || $1 == "mmap_write" ]]; then
-	echo "==== create testfile ===="
-	fallocate -l 100M testfile
-	cat /proc/meminfo | grep "file"
+echo "==== create testfile ===="
+fallocate -l 100M testfile
+cat /proc/meminfo | grep "file"
 
-	echo "==== first $1 ===="
-	./a.out $1
-	cat /proc/meminfo | grep "file"
+echo "==== first call demo, $1 ===="
+./a.out $1
 
-	echo "==== second $1 ===="
-	./a.out $1
-	cat /proc/meminfo | grep "file"
+echo "==== second call demo, $1 ===="
+./a.out $1
 
-	rm -fr testfile
-fi
+rm -fr testfile
