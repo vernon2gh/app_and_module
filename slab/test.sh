f1f51ca1d063893bfece141157e7a58518e3f5a8
Vernon Yang <vernon2gm@gmail.com>
2024-07-10 21:55:49 +0800

    Refine slab test shell
    Signed-off-by: Vernon Yang <vernon2gm@gmail.com>

--- slab/test.sh
+++ slab/test.sh
@@ -1,13 +1,39 @@
 #!/bin/bash
 
-echo "==== origin slab log ===="
-cat /proc/meminfo | grep -E 'SReclaimable|SUnreclaim'
-cat /proc/slabinfo | grep '^kmalloc.*4k'
+compare_values() {
+ 	if [ $# -ne 4 ]; then
+		echo "Usage: compare_values file1 file2 column delta"
+		return 1
+	fi
+
+	diff -y --suppress-common-lines $1 $2 | awk -v column=$3 -v delta=$4 '
+	function abs(x) {
+		return (x < 0 ? -x : x);
+	}
+	{
+                split($0, array, "|");
+                split(array[1], left, " ");
+                val1 = left[column];
+                split(array[2], right, " ");
+                val2 = right[column];
+
+		if (abs(val2 - val1) > delta)
+			print $0;
+	}
+	'
+}
+
+cat /proc/meminfo  > origin_meminfo.txt
+cat /proc/slabinfo > origin_slabinfo.txt
 
 insmod mod.ko demo_entry="$1"
 
-echo "==== slab log after access ===="
-cat /proc/meminfo | grep -E 'SReclaimable|SUnreclaim'
-cat /proc/slabinfo | grep '^kmalloc.*4k'
+cat /proc/meminfo  > access_meminfo.txt
+cat /proc/slabinfo > access_slabinfo.txt
 
 rmmod mod.ko
+
+compare_values origin_meminfo.txt access_meminfo.txt 2 100000
+compare_values origin_slabinfo.txt access_slabinfo.txt 2 25500
+
+rm -fr origin_*.txt access_*.txt
