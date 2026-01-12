03bd741c6808f8bc02eec72c8b2ce7525ffa8084
Vernon Yang <vernon2gm@gmail.com>
2026-01-12 11:41:53 +0800

    refine mem_ext
    Signed-off-by: Vernon Yang <vernon2gm@gmail.com>

--- mem_ext/app.c
+++ mem_ext/app.c
@@ -1,34 +1,33 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <errno.h>
+#include <string.h>
 #include <sys/mman.h>
 
-#define LEN (16 * 1024 * 1024) /* 16MB */
+#define PAGE_SIZE	4096
+#define BATCH_SIZE	(128 * 1024 * 1024)	// 128MB
+#define BATCH_PAGES	(BATCH_SIZE / PAGE_SIZE)
 
 int main(int argc, char *argv[])
 {
-	char *buf;
-	int err, i;
+	char *buffer;
+	int err;
 
+	printf("Current pid: %d\n", getpid());
+	printf("Batch size : %d MB\n", BATCH_SIZE / (1024 * 1024));
+	printf("Batch pages: %d(4KB) or %d(2MB)\n", BATCH_PAGES,
+				BATCH_SIZE / (2 * 1024 * 1024));
 	sleep(1); /* wait ebpf to attach this task */
 
-	buf = mmap(NULL, LEN, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
-	if (buf == MAP_FAILED)
+	buffer = mmap(NULL, BATCH_SIZE, PROT_READ | PROT_WRITE,
+				MAP_PRIVATE | MAP_ANON, -1, 0);
+	if (buffer == MAP_FAILED)
 		return -EINVAL;
-	// printf("pid %d, buf %lx\n", getpid(), (unsigned long)buf);
 
-	err = madvise(buf, LEN, MADV_HUGEPAGE);
-	if (err == -1)
-		goto unmap;
+	memset(buffer, 't', BATCH_SIZE);
 
-	// printf("wait trigger.\n");
-	// getchar();
-
-	for (i = 0; i < LEN; i += 4096)
-		buf[i] = 1;
-
-	// getchar();
 unmap:
-	munmap(buf, LEN);
+	munmap(buffer, BATCH_SIZE);
+
 	return 0;
 }
--- mem_ext/test.sh
+++ mem_ext/test.sh
@@ -1,12 +1,60 @@
 #!/bin/bash
 
-DIR=./build/bin
-gcc app.c
+BPF=./build/bin
+UB=~/UnixBench
 
-## origin test
-perf stat -- ./a.out
+echo always > /sys/kernel/mm/transparent_hugepage/enabled
 
-## thp adjust test
-$DIR/mthp_adjust &
-perf stat -- ./a.out
-killall mthp_adjust
+function test_a.out()
+{
+	## origin test
+	perf stat -- ./a.out
+
+	## thp adjust test
+	$BPF/mthp_adjust &
+	sleep 3
+	perf stat -- ./a.out
+	killall mthp_adjust
+}
+
+function test_stream()
+{
+	export OMP_NUM_THREADS=8
+
+	## origin test
+	./stream
+
+	## thp adjust test
+	$BPF/mthp_adjust &
+	sleep 3
+	./stream
+	killall mthp_adjust
+}
+
+function test_unixbench()
+{
+	cd $UB
+
+	## origin test
+	sleep 60
+	echo 3 > /proc/sys/vm/drop_caches
+	./Run -c 1 shell8
+
+	## thp adjust test
+	sleep 60
+	echo 3 > /proc/sys/vm/drop_caches
+	$BPF/mthp_adjust &
+	sleep 3
+	./Run -c 1 shell8
+	killall mthp_adjust
+
+	cd -
+}
+
+make
+
+test_a.out
+## test_stream
+## test_unixbench
+
+make clean
