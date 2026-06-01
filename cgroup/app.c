e7555e0a31cc2b70807b2212edaf13904870378d
Vernon Yang <vernon2gm@gmail.com>
2026-06-01 14:08:32 +0800

    cgroup: add mmap_touch_free demo
    Signed-off-by: Vernon Yang <vernon2gm@gmail.com>

--- cgroup/app.c
+++ cgroup/app.c
@@ -13,11 +13,13 @@ static int pagesize;
 
 enum demo_entry {
 	DEMO_MEMORY_RECLAIM_SWAPPINESS,
+	DEMO_MMAP_TOUCH_FREE,
 	DEMO_ENTRY_MAX,
 };
 
 static char *string[DEMO_ENTRY_MAX] = {
 	"memory_reclaim_swappiness",
+	"mmap_touch_free",
 };
 
 static enum demo_entry test_demo_entry(char *entry)
@@ -51,6 +53,29 @@ static void memory_reclaim_swappiness(void)
 	munmap(buf, pagesize * nr);
 }
 
+static void mmap_touch_free(void)
+{
+#define MMAP_SIZE	(120UL * 1024 * 1024)
+#define STEP_SIZE	(10UL * 1024 * 1024)
+	unsigned long offset;
+	char *buf;
+
+	buf = mmap(0, MMAP_SIZE, PROT_READ | PROT_WRITE,
+		   MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+
+	for (offset = 0; offset < MMAP_SIZE; offset += STEP_SIZE) {
+		memset(buf + offset, 't', STEP_SIZE);
+		sleep(1);
+	}
+
+	for (offset = 0; offset < MMAP_SIZE; offset += STEP_SIZE) {
+		madvise(buf + offset, STEP_SIZE, MADV_DONTNEED);
+		sleep(1);
+	}
+
+	munmap(buf, MMAP_SIZE);
+}
+
 int main(int argc, char *argv[])
 {
 	pagesize = getpagesize();
@@ -60,6 +85,9 @@ int main(int argc, char *argv[])
 			break;
 		case DEMO_MEMORY_RECLAIM_SWAPPINESS:
 			memory_reclaim_swappiness();
+			break;
+		case DEMO_MMAP_TOUCH_FREE:
+			mmap_touch_free();
 			break;
 	}
 
